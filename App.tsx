98c74e8038f76c118041acb1c44a557a7880c161
KubilayKerim <k-kubilay@hotmail.com>
2022-04-10 14:42:38 +0300

    project created

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+        <View>
+          <Text>Translate App</Text>
+        </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
